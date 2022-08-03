370d38ede7d8ea76a2251de9fcb277d94e73e9c6
Алексей Шевченко <alex18w@mail.ru>
2022-08-03 21:44:54 +0500

    Задача 24

---
+++ Stminar_4/Program.cs
@@ -0,0 +1,17 @@
+﻿void Zadacha_24()
+// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А
+{
+    Console.WriteLine("Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А");
+    Console.WriteLine("Введите число ");
+    int number = Convert.ToInt32(Console.ReadLine()); 
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    Console.WriteLine($"Сумма чисел от 1 до {number} равна {sum}");
+}
+
+
+Zadacha_24();
